38ff40d21bb931a0b4dcafe0646c52f372f1d29d
David Aleu Mosegui <david.aleu@raona.com>
2019-05-03 21:39:40 +0200

    first commit

---
+++ client/src/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style/index.css';
+import WeCooltra from './components/weCooltra/WeCooltra';
+
+ReactDOM.render(<WeCooltra />, document.getElementById('root'));
