e7619c6c2c4212d6e8603484198d8193924d6c6d
Albert Suarez <alsumo95@gmail.com>
2019-05-04 11:24:52 +0200

    Fixed error with timestamp creation

--- api/src/util/date.py
+++ api/src/util/date.py
@@ -2,4 +2,4 @@ import datetime
 
 
 def to_string(str_to_parse):
-    return int(datetime.datetime.strptime(str_to_parse, "%Y-%m-%d %H:%M:%S").timestamp())
+    return datetime.datetime.strptime(str_to_parse, "%Y-%m-%d %H:%M:%S")
